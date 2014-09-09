941a917e69a6e699812c4162e22beb1f64839939
Choups314 <barnabe.chauvin@gmail.com>
2014-09-09 20:59:59 +0200

    Add the main.c of the generator :    read lines from several files.

---
+++ generator/main.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define KEY_LENGTH 600
+
+#define openFile(var, filename) (var) = fopen((filename), "r"); \
+				if((var) == NULL) \
+					printf("%s : no such file ...", (filename)); 
+
+#define closeFiles(var, numFiles) for(i = 0 ; i < (numFiles) ; i++)  \
+					fclose((var)[i]); \
+				  free((var));
+
+static unsigned char *key = NULL;
+
+int genKey(int length);
+
+int main(int argc, char **argv)
+{
+	FILE **inputFiles = NULL;
+	int i, numInputs = 0, inputProcessed = 0;
+	char **inputs = NULL, *tmpBuffer = NULL;
+	size_t bufferLength;
+	ssize_t *inputsLength;
+
+	srand(time(NULL));
+	genKey(KEY_LENGTH);
+
+	if(argc < 2) {
+		printf("Usage : generator <input_file1> <input_file2> ...\n");
+		return -1;
+	}
+
+	inputFiles = (FILE**) malloc(sizeof(FILE*) * (argc - 1));
+	if(inputFiles == NULL) {
+		printf("Out of memory ...\n");
+		return -1;
+	}
+
+	// First, count the number of lines
+	for(i = 1 ; i < argc ; i++) {
+		openFile(inputFiles[i - 1], argv[i]);
+		if(inputFiles[i - 1] == NULL)
+			continue;
+		while(getline(&tmpBuffer, &bufferLength, inputFiles[i - 1]) != -1) {
+			numInputs++;
+		}
+	}
+
+	inputs = (char**) malloc(sizeof(char*) * numInputs);
+	if(inputs == NULL) {
+		closeFiles(inputFiles, argc - 1);
+		printf("Out of memory ...i\n");
+		return -1;
+	}
+	inputsLength = (ssize_t*) malloc(sizeof(ssize_t) * numInputs);
+	if(inputsLength == NULL) {
+		closeFiles(inputFiles, argc - 1);
+		free(inputs);
+		printf("Out of memory ... \n");
+		return -1;
+	}
+
+	// Then read lines
+	for(i = 1 ; i < argc ; i++) {
+		if(inputFiles[i - 1] == NULL)
+			continue;
+		while((inputProcessed < numInputs) && (inputsLength[inputProcessed] = getline(&inputs[inputProcessed], &bufferLength, inputFiles[i - 1])) != -1) {
+			inputProcessed++;
+		}
+	}
+
+
+	// And finaly apply XOR crypt ...
+	// TODO
+
+	closeFiles(inputFiles, argc - 1);
+	for(i = 1 ; i < numInputs ; i++)
+		free(inputs[i]);
+	free(inputs);
+	free(inputsLength);
+
+	if(key != NULL) {
+		free(key);
+	}
+
+	return 0;
+}
+
+int genKey(int length)
+{
+	int i;
+
+	key = (unsigned char*) malloc(length);
+	if(key == NULL) {
+		printf("Out of memory ... \n");
+		return -1;
+	}
+	for(i = 0 ; i < length ; i++) {
+		key[i] = rand() % 256;
+	}
+	return 1;
+}
+
