4f6c67d97fed9ef1a578ef064b196ccd1efb8a2e
Choups314 <barnabe.chauvin@gmail.com>
2015-01-02 20:42:55 +0100

    Process speed drastically increased ! It can now decrypt any message length. However, still encountering some bugs with specials characters (Like '), and cannot decrypt upper/case. I still need to take the message length in account.

--- main.py
+++ main.py
@@ -7,6 +7,36 @@ def isPrintable(character) :
 		return True
 	return False 
 
+# Return True if pattern in tab (Including the offsets) 
+def tabMatch(tab, pattern, tabOffset, patternOffset, length): 
+	for i in xrange(length): 
+		if tab[i + tabOffset] != pattern[i + patternOffset]:
+			return False
+	return True
+
+# grid is expected to be a non-empty 3D list
+# This function returns the common value of this level (x) : [][x][index - length]
+# (If several are found, it returns the first one)
+def findIntersection(grid, index, length):
+	# We take the first message as a reference, but it could very well be another.
+	for patternRaw in grid[0]:
+		pattern = patternRaw[index:(index+length)]
+		# We now check if this pattern is present in the other messages
+		presentInAll = True
+		for i in xrange(1, len(grid)):
+			patternMatches = 0 		# Number of time the pattern is present
+			for word in grid[i]:
+				present = tabMatch(word, pattern, index, 0, length)
+				if present: 
+					patternMatches += 1
+			if (patternMatches == 0):	# It MUST be present in every messages
+				presentInAll = False
+				break
+		if presentInAll:
+			return pattern
+	print("No common parts !") # At least in the first message ..
+	return []
+
 # First, parse the dictionary file !  print("Parse dictionary ...")
 dico = {} 
 fd = open("wordsEn.txt", "r")
@@ -74,8 +104,9 @@ def makeWord(msgId, offset, currentWord, dicoPosition, wordLength):
 	global words
 	global dico
 	if(offset >= len(mes.messages[msgId])):
-		print(currentWord + " ..................")
-		return 0
+#		print(currentWord + " ..................")
+		return -1
+	possibilities = []
 	for i in xrange(len(key[offset])) :
 		character = decrypt[offset][i][msgId]
 		if (character >= 65 and character <= 90) : # Change to lower case if needed		
@@ -84,13 +115,52 @@ def makeWord(msgId, offset, currentWord, dicoPosition, wordLength):
 			character = 32			# Space
 		if character in dicoPosition :
 			if character == 32:
-				print(currentWord + str(msgId))
-				makeWord(msgId, offset + 1, currentWord + chr(decrypt[offset][i][msgId]), dico, 0)
+				# The first cell of each words stores the word's offset in the message
+				possibilities.append([offset, wordLength + 1] + currentWord + [key[offset][i]])
 			else:
-				makeWord(msgId, offset + 1, currentWord + chr(character), dicoPosition[character], wordLength + 1)
-			
+				possibilities.extend(makeWord(msgId, offset + 1, currentWord + [key[offset][i]], dicoPosition[character], wordLength + 1))
+	return possibilities
 
-# Now, try to find the first word
+def extendWord(msgId, currentWord):
+	global dico
+	currentLength = currentWord[1]
+	currentOffset = currentWord[0]
+	nextPossibilities = makeWord(msgId, currentOffset + 1, [], dico, 0)
+	if(nextPossibilities == -1):	# We reach the end of the message ..
+		print("ENNNNNNNNNNNNNNNND")
+	nextWords = []
+	for pos in nextPossibilities:
+		nextWord = [pos[0], currentLength + pos[1]] + currentWord[2:] + pos[2:]
+		nextWords.append(nextWord)
+	return nextWords
+
+# Now, get every possibilities for the first word
+# (By the way, it initialize the words list)
 for k in xrange(len(mes.messages)) :
-	makeWord(k, 0, "", dico, 0)
+	words.append(makeWord(k, 0, [], dico, 0))
 
+# i is the index in the key
+i = 0
+previous = (0, 0)
+foundKey = []
+while i < 30:
+	shortestWord = words[0][0][1] - i		# ! This value depend on the current offset !
+	for j in xrange(len(words)):		#len(words) is the number of messages
+		# First, make sure this word passed the previous loop (i.e. match the begining of the found key)
+		# Then, make sure we have enough characters ..
+		newWords = []
+		for word in words[j]:
+			if (tabMatch(word, foundKey, previous[0] + 2, previous[0], previous[1])):
+				if word[1] <= i:	# Remember, the second cell is the word length
+					newWords.extend(extendWord(j, word))
+				else:
+					newWords.append(word)
+		words[j] = newWords
+		for word in words[j]:
+			if (word[1] - i < shortestWord): 
+				shortestWord = word[1] - i
+	foundKey.extend(findIntersection(words, i + 2, shortestWord))
+	previous = (i, shortestWord)
+	i += shortestWord
+			
+print(foundKey)
